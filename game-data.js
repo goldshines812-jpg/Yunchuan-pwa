fc6a386cd73493427a607d73fd9e80218bb7b378
goldshines812-jpg <goldshines812@gmail.com>
2026-09-18 19:40:26 +0800

    Create game-data.js

---
+++ game-data.js
@@ -0,0 +1,22 @@
+const GAME_DATA = {
+  boy: [
+    { name: "妍妍 💄", id: "yanyan" },
+    { name: "小雨 🌸", id: "xiaoyu" },
+    { name: "娜娜 ✨", id: "nana" }
+  ],
+
+  girl: [
+    { name: "阿哲 😎", id: "azhe" },
+    { name: "子豪 ☕", id: "zihao" },
+    { name: "小宇 🎧", id: "xiaoyu-boy" }
+  ],
+
+  free: [
+    { name: "妍妍 💄", id: "yanyan" },
+    { name: "小雨 🌸", id: "xiaoyu" },
+    { name: "娜娜 ✨", id: "nana" },
+    { name: "阿哲 😎", id: "azhe" },
+    { name: "子豪 ☕", id: "zihao" },
+    { name: "小宇 🎧", id: "xiaoyu-boy" }
+  ]
+};
